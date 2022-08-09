1c9254477f9043321057395a2935cd2f3c6d59bc
meyadavajeet <meyadavajeet@gmail.com>
2022-08-09 19:16:34 +0530

    changes on connection and readme added

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
 
 
-const connectDbs = async () =>{
+const connectDbs = async () => {
     try {
         const url = process.env.MONGO_ATLAS_URL
-        const conn = await mongoose.connect(url,{
-            // useCreateIndex: true,
-            // useUnifiedTopology: true,
-            // useNewUrlParser: true,
-            // useFindAndModify: false,
+        const conn = await mongoose.connect(url, { useNewUrlParser: true }, () => {
+            console.log(`Mongodb Database Connected!${ process.env.SERVER_PORT}`.bgGreen.white);
         });
-        console.log(`Mongodb Database Connected!${conn.connection.host}`.bgGreen.white);
     } catch (error) {
         console.error(`error${error.message}`.bgRed.white);
     }
