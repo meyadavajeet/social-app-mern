a5af340ccc450eeed04875838e955d74253bd7b4
meyadavajeet <meyadavajeet@gmail.com>
2022-08-13 23:31:13 +0530

    timeline post api

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Post = require('../models/Post');
+const User = require('../models/User');
 
 /**
  * postController
@@ -61,8 +62,28 @@ router.delete("/:id", async (req, res) => {
 });
 
 /**
- * like a post
+ * like & dislike a post
  */
+router.put("/:id/like-dislike", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).json({ "message": "Post not found" });
+        }
+        //like post
+        if (!post.likes.includes(req.body.userId)) {
+            await post.updateOne({ $push: { likes: req.body.userId } });
+            return res.status(200).json({ "message": "The post has been liked" });
+        } else {
+            //dislike post
+            await post.updateOne({ $pull: { likes: req.body.userId } });
+            return res.status(200).json({ "message": "The post has been disliked" });
+        }
+    } catch (err) {
+        return res.status(500).json({ "message": err });
+    }
+
+})
 
 /**
  * get a post
@@ -87,7 +108,21 @@ router.get("/:id", async (req, res) => {
 
 
 /**
- * get timeline post of user
+ * get timeline post of user along with post of the user you follow
  */
+router.post("/timeline/all", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPost = await Post.find({ userId: currentUser._id });
+        const friendPosts = await Promise.all(
+            currentUser.following.map((friendId) => {
+                return Post.find({ userId: friendId });
+            })
+        );
+        res.json(userPost.concat(...friendPosts));
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+});
 
 module.exports = router;
