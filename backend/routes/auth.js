ea5c35635b5a42dbe23bab9ddf2a3d54129a924f
meyadavajeet <meyadavajeet@gmail.com>
2022-08-13 18:32:47 +0530

    changes in the response json format

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -13,20 +13,35 @@ const bcrypt = require('bcrypt');
  */
 router.post("/register", async (req, res) => {
     try {
+        //get data from request body
+        const email = req.body.email;
+        const username = req.body.username;
+
+        // check user already registered or not
+        const checkUser = await User.find({ email: email });
+        if (checkUser.length > 0) {
+            return res.status(409).json({ 'message': `User with ${email} already Exist!!` });
+        }
+        const checkUserName = await User.find({ username: username });
+        if (checkUserName.length > 0) {
+            return res.status(409).json({ 'message': `User with ${username} already Exist!!` });
+        }
+
         //genrate hashed password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
         //create new user
         const newUser = await new User({
-            username: req.body.username,
+            username: username,
             password: hashedPassword,
-            email: req.body.email,
+            email: email,
         });
         //save new user
         const user = await newUser.save();
-        res.status(200).json(user);
+        return res.status(200).json({ 'data': user });
     } catch (error) {
-        res.status(500).json(err);
+        return res.status(500).json({ "message": err.message });
     }
 });
 
@@ -38,13 +53,13 @@ router.post("/login", async (req, res) => {
         let user = await User.findOne({
             email: req.body.email,
         });
-        !user && res.status(404).json("user not found");
+        !user && res.status(404).json({ "message": "user not found" });
         const validPassword = await bcrypt.compare(req.body.password, user.password);
-        !validPassword && res.status(400).json("Wrong password!!")
+        !validPassword && res.status(400).json({ "message": "Wrong password!!" })
 
-        res.status(200).json(user);
+        return res.status(200).json({ 'data': user });
     } catch (err) {
-       res.status(500).json(err);
+        return res.status(500).json({ 'message': err.message });
     }
 });
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -14,6 +14,15 @@ router.get("/", (req, res) => {
 /**
  * get a user
  */
+router.get(":/id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, updatedAt, ...others } = user._doc;
+        return res.status(200).json({ "data": others });
+    } catch (err) {
+        return res.status(500).json({ "message": err });
+    }
+});
 
 /**
  * update user
@@ -25,21 +34,21 @@ router.put("/:id", async (req, res) => {
                 const salt = await bcrypt.genSalt(10);
                 req.body.password = await bcrypt.hash(req.body.password, salt);
             } catch (err) {
-                return res.status(500).json(err);
+                return res.status(500).json({ "message": err });
             }
         }
         try {
             const user = await User.findByIdAndUpdate(req.params.id, {
                 $set: req.body,
             });
-            return res.status(200).json("Account has been updated successfully");
+            return res.status(200).json({ "message": "Account has been updated successfully" });
 
         } catch (err) {
-            return res.status(500).json(err);
+            return res.status(500).json({ "message": err });
         }
 
     } else {
-        return res.status(403).json("You can only update your account");
+        return res.status(403).json({ "message": "You can only update your account" });
     }
 });
 
@@ -48,18 +57,18 @@ router.put("/:id", async (req, res) => {
 /**
  * delete user
  */
- router.delete("/:id", async (req, res) => {
-    if(req.body.userId === req.params.id || req.body.isAdmin){
-        try{
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
             const user = await User.findByIdAndDelete(req.params.id);
-            return res.status(200).json("Accont has been deleted successfully!!");
-        }catch(err){
-            return res.status(500).json(err);
+            return res.status(200).json({ "message": "Accont has been deleted successfully!!" });
+        } catch (err) {
+            return res.status(500).json({ "message": err });
         }
-    }else{
-        return res.status(403).json("You can delete only your account!!");
+    } else {
+        return res.status(403).json({ "message": "You can delete only your account!!" });
     }
- });
+});
 
 
 /**
