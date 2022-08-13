af772e1fe083f7c745163ac6ad962c92fbb3bf02
meyadavajeet <meyadavajeet@gmail.com>
2022-08-13 15:36:14 +0530

    working on delete user rest api

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -19,7 +19,7 @@ router.get("/", (req, res) => {
  * update user
  */
 router.put("/:id", async (req, res) => {
-    if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
         if (req.body.password) {
             try {
                 const salt = await bcrypt.genSalt(10);
@@ -48,6 +48,19 @@ router.put("/:id", async (req, res) => {
 /**
  * delete user
  */
+ router.delete("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        try{
+            const user = await User.findByIdAndDelete(req.params.id);
+            return res.status(200).json("Accont has been deleted successfully!!");
+        }catch(err){
+            return res.status(500).json(err);
+        }
+    }else{
+        return res.status(403).json("You can delete only your account!!");
+    }
+ });
+
 
 /**
  * follow a user
