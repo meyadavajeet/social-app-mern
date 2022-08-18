161cf7c5fe45839e203f5549ecbb55a6931b45b6
meyadavajeet <meyadavajeet@gmail.com>
2022-08-18 09:48:43 +0530

    assests and material icons packages installed

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.css';
+// import {HomeIcon} from '@mui/icons-material';
+
+const Home = () => {
+    return (
+        <>
+            {/* <HomeIcon/> */}
+            Welcome to Home Page
+        </>
+    )
+}
+
+export default Home
